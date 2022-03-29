81113020eaa9be5be05b422f8ac01b0842f99bae
joeelmahallawy <youssef.elmahallawy01@gmail.com>
2022-03-28 21:52:35 -0400

    changed endpoint for returning json

--- pages/api/getConvo.ts
+++ pages/api/getConvo.ts
@@ -4,15 +4,16 @@ import fs from "fs";
 
 const getConversation = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const { query } = req;
+    // const { query } = req;
 
     // read json
     const data = fs.readFileSync(`./conversations.json`);
     // turn to string
-    const first = data.toString();
+    // const first = data.toString();
     // parse and grab key
-    // @ts-expect-error
-    res.json(JSON.parse(first)[query.q]);
+    res.json(data);
+
+    // res.json(JSON.parse(first)[query.q]);
   } catch (err) {
     res.status(err.status).json({ err: err.message });
   }
